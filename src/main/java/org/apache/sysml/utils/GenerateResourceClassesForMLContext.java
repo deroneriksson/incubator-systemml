32a1ca49fc589d6d440609bf3948c881bca11317
Deron Eriksson <deron@us.ibm.com>
2017-04-20 17:27:15 -0700

    Create function output classes

--- src/main/java/org/apache/sysml/utils/GenerateResourceClassesForMLContext.java
+++ src/main/java/org/apache/sysml/utils/GenerateResourceClassesForMLContext.java
@@ -25,7 +25,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -33,11 +32,14 @@ import java.util.Map;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.sysml.api.DMLScript;
+import org.apache.sysml.api.mlcontext.MLContext;
 import org.apache.sysml.api.mlcontext.MLResults;
 import org.apache.sysml.api.mlcontext.Script;
 import org.apache.sysml.api.mlcontext.ScriptExecutor;
 import org.apache.sysml.parser.DMLProgram;
 import org.apache.sysml.parser.DataIdentifier;
+import org.apache.sysml.parser.Expression.DataType;
+import org.apache.sysml.parser.Expression.ValueType;
 import org.apache.sysml.parser.FunctionStatement;
 import org.apache.sysml.parser.FunctionStatementBlock;
 import org.apache.sysml.parser.LanguageException;
@@ -47,7 +49,9 @@ import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtConstructor;
+import javassist.CtField;
 import javassist.CtMethod;
+import javassist.CtNewConstructor;
 import javassist.CtNewMethod;
 import javassist.Modifier;
 import javassist.NotFoundException;
@@ -63,12 +67,16 @@ public class GenerateResourceClassesForMLContext {
 
 	public static final String SOURCE = "scripts";
 	public static final String DESTINATION = "target/classes";
-	public static final String BASE_DEST_PACKAGE = "org.apache.sysml.api.mlcontext.resources";
+	// public static final String BASE_DEST_PACKAGE =
+	// "org.apache.sysml.api.mlcontext.resources";
+	public static final String BASE_DEST_PACKAGE = "org.apache.sysml";
 	public static final String CONVENIENCE_BASE_DEST_PACKAGE = "org.apache.sysml.api.mlcontext.convenience";
 	public static final String PATH_TO_MLCONTEXT_CLASS = "org/apache/sysml/api/mlcontext/MLContext.class";
 	public static final String PATH_TO_MLRESULTS_CLASS = "org/apache/sysml/api/mlcontext/MLResults.class";
 	public static final String PATH_TO_SCRIPT_CLASS = "org/apache/sysml/api/mlcontext/Script.class";
 	public static final String PATH_TO_SCRIPTTYPE_CLASS = "org/apache/sysml/api/mlcontext/ScriptType.class";
+	public static final String PATH_TO_MATRIX_CLASS = "org/apache/sysml/api/mlcontext/Matrix.class";
+	public static final String PATH_TO_FRAME_CLASS = "org/apache/sysml/api/mlcontext/Frame.class";
 
 	public static String source = SOURCE;
 	public static String destination = DESTINATION;
@@ -90,11 +98,30 @@ public class GenerateResourceClassesForMLContext {
 			System.out.println("************************************");
 			System.out.println("Source: " + source);
 			System.out.println("Destination: " + destination);
+			makeCtClasses();
 			recurseDirectoriesForClassGeneration(source);
 			String fullDirClassName = recurseDirectoriesForConvenienceClassGeneration(source);
 			addConvenienceMethodsToMLContext(source, fullDirClassName);
 		} finally {
 			DMLScript.VALIDATOR_IGNORE_ISSUES = false;
+		}
+	}
+
+	public static void makeCtClasses() {
+		try {
+			ClassPool pool = ClassPool.getDefault();
+			pool.makeClass(new FileInputStream(new File(destination + File.separator + PATH_TO_MLCONTEXT_CLASS)));
+			pool.makeClass(new FileInputStream(new File(destination + File.separator + PATH_TO_MLRESULTS_CLASS)));
+			pool.makeClass(new FileInputStream(new File(destination + File.separator + PATH_TO_SCRIPT_CLASS)));
+			pool.makeClass(new FileInputStream(new File(destination + File.separator + PATH_TO_SCRIPTTYPE_CLASS)));
+			pool.makeClass(new FileInputStream(new File(destination + File.separator + PATH_TO_MATRIX_CLASS)));
+			pool.makeClass(new FileInputStream(new File(destination + File.separator + PATH_TO_FRAME_CLASS)));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -110,10 +137,8 @@ public class GenerateResourceClassesForMLContext {
 	 */
 	public static void addConvenienceMethodsToMLContext(String source, String fullDirClassName) {
 		try {
-			File mlContextClassFile = new File(destination + File.separator + PATH_TO_MLCONTEXT_CLASS);
-			InputStream is = new FileInputStream(mlContextClassFile);
 			ClassPool pool = ClassPool.getDefault();
-			CtClass ctMLContext = pool.makeClass(is);
+			CtClass ctMLContext = pool.get(MLContext.class.getName());
 
 			CtClass dirClass = pool.get(fullDirClassName);
 			String methodName = convertFullClassNameToConvenienceMethodName(fullDirClassName);
@@ -460,11 +485,7 @@ public class GenerateResourceClassesForMLContext {
 			ClassPool pool = ClassPool.getDefault();
 			CtClass ctNewScript = pool.makeClass(fullScriptClassName);
 
-			File scriptClassFile = new File(destination + File.separator + PATH_TO_SCRIPT_CLASS);
-			InputStream is = new FileInputStream(scriptClassFile);
-			CtClass ctScript = pool.makeClass(is);
-			pool.makeClass(new FileInputStream(new File(destination + File.separator + PATH_TO_SCRIPTTYPE_CLASS)));
-
+			CtClass ctScript = pool.get(Script.class.getName());
 			ctNewScript.setSuperclass(ctScript);
 
 			CtConstructor ctCon = new CtConstructor(null, ctNewScript);
@@ -482,6 +503,8 @@ public class GenerateResourceClassesForMLContext {
 			e.printStackTrace();
 		} catch (CannotCompileException e) {
 			e.printStackTrace();
+		} catch (NotFoundException e) {
+			e.printStackTrace();
 		}
 
 	}
@@ -497,6 +520,7 @@ public class GenerateResourceClassesForMLContext {
 		String scriptString = fileToString(scriptFilePath);
 		Script script = new Script(scriptString);
 		ScriptExecutor se = new ScriptExecutor() {
+			@Override
 			public MLResults execute(Script script) {
 				setup(script);
 				parseScript();
@@ -541,11 +565,7 @@ public class GenerateResourceClassesForMLContext {
 					FunctionStatement fs = (FunctionStatement) st;
 
 					String dmlFunctionCall = generateDmlFunctionCall(scriptFilePath, fs);
-					String functionCallMethod = generateFunctionCallMethod(scriptFilePath, fs, dmlFunctionCall);
-
-					ClassPool pool = ClassPool.getDefault();
-					pool.makeClass(
-							new FileInputStream(new File(destination + File.separator + PATH_TO_MLRESULTS_CLASS)));
+					String functionCallMethod = generateFunctionCallMethodNew(scriptFilePath, fs, dmlFunctionCall);
 
 					CtMethod m = CtNewMethod.make(functionCallMethod, ctNewScript);
 					ctNewScript.addMethod(m);
@@ -557,10 +577,6 @@ public class GenerateResourceClassesForMLContext {
 		} catch (LanguageException e) {
 			System.out.println("Could not add function methods for " + ctNewScript.getName());
 		} catch (CannotCompileException e) {
-			System.out.println("Could not add function methods for " + ctNewScript.getName());
-		} catch (FileNotFoundException e) {
-			System.out.println("Could not add function methods for " + ctNewScript.getName());
-		} catch (IOException e) {
 			System.out.println("Could not add function methods for " + ctNewScript.getName());
 		} catch (RuntimeException e) {
 			System.out.println("Could not add function methods for " + ctNewScript.getName());
@@ -592,9 +608,9 @@ public class GenerateResourceClassesForMLContext {
 
 		try {
 
-			if ((fs.getInputParams().size() == 0) && (full == false)) {
-				return;
-			}
+			// if ((fs.getInputParams().size() == 0) && (full == false)) {
+			// return;
+			// }
 
 			int bl = fs.getBeginLine();
 			int el = fs.getEndLine();
@@ -655,11 +671,272 @@ public class GenerateResourceClassesForMLContext {
 		sb.append("public String ");
 		sb.append(fs.getName());
 		if (full) {
+			sb.append("__");
+		} else {
 			sb.append("_");
 		}
 		sb.append("() {\n");
 		sb.append("String docString = \"" + functionString + "\";\n");
 		sb.append("return docString;\n");
+		sb.append("}\n");
+		return sb.toString();
+	}
+
+	public static String getParamTypeAsString(DataIdentifier param) {
+		DataType dt = param.getDataType();
+		ValueType vt = param.getValueType();
+		if ((dt == DataType.SCALAR) && (vt == ValueType.INT)) {
+			return "long";
+		} else if ((dt == DataType.SCALAR) && (vt == ValueType.DOUBLE)) {
+			return "double";
+		} else if ((dt == DataType.SCALAR) && (vt == ValueType.BOOLEAN)) {
+			return "boolean";
+		} else if ((dt == DataType.SCALAR) && (vt == ValueType.STRING)) {
+			return "String";
+		} else if (dt == DataType.MATRIX) {
+			return "org.apache.sysml.api.mlcontext.Matrix";
+		} else if (dt == DataType.FRAME) {
+			return "org.apache.sysml.api.mlcontext.Frame";
+		}
+		return null;
+	}
+
+	public static String getSimpleParamTypeAsString(DataIdentifier param) {
+		DataType dt = param.getDataType();
+		ValueType vt = param.getValueType();
+		if ((dt == DataType.SCALAR) && (vt == ValueType.INT)) {
+			return "long";
+		} else if ((dt == DataType.SCALAR) && (vt == ValueType.DOUBLE)) {
+			return "double";
+		} else if ((dt == DataType.SCALAR) && (vt == ValueType.BOOLEAN)) {
+			return "boolean";
+		} else if ((dt == DataType.SCALAR) && (vt == ValueType.STRING)) {
+			return "String";
+		} else if (dt == DataType.MATRIX) {
+			return "Matrix";
+		} else if (dt == DataType.FRAME) {
+			return "Frame";
+		}
+		return null;
+	}
+
+	public static String getFullFunctionOutputClassName(String scriptFilePath, FunctionStatement fs) {
+		String p = scriptFilePath;
+		p = p.replace("-", "_");
+		p = p.replace(File.separator, ".");
+		p = p.toLowerCase();
+		p = p.substring(0, p.lastIndexOf("."));
+
+		String c = fs.getName();
+		c = c.substring(0, 1).toUpperCase() + c.substring(1);
+		c = c + "_output";
+
+		String functionOutputClassName = BASE_DEST_PACKAGE + "." + p + "." + c;
+		return functionOutputClassName;
+	}
+
+	public static void createFunctionOutputClass(String scriptFilePath, FunctionStatement fs) {
+
+		try {
+			ArrayList<DataIdentifier> oparams = fs.getOutputParams();
+			if ((oparams.size() == 0) || (oparams.size() == 1)) {
+				return;
+			}
+
+			String fullFunctionOutputClassName = getFullFunctionOutputClassName(scriptFilePath, fs);
+			System.out.println("Generating Class: " + fullFunctionOutputClassName);
+			ClassPool pool = ClassPool.getDefault();
+			CtClass ctFuncOut = pool.makeClass(fullFunctionOutputClassName);
+
+			// add fields
+			for (int i = 0; i < oparams.size(); i++) {
+				DataIdentifier oparam = oparams.get(i);
+				String type = getParamTypeAsString(oparam);
+				String name = oparam.getName().toLowerCase();
+				String fstring = "public " + type + " " + name + ";";
+				CtField field = CtField.make(fstring, ctFuncOut);
+				ctFuncOut.addField(field);
+			}
+
+			// add constructor
+			String simpleFuncOutClassName = fullFunctionOutputClassName
+					.substring(fullFunctionOutputClassName.lastIndexOf(".") + 1);
+			StringBuilder con = new StringBuilder();
+			con.append("public " + simpleFuncOutClassName + "(");
+			for (int i = 0; i < oparams.size(); i++) {
+				if (i > 0) {
+					con.append(", ");
+				}
+				DataIdentifier oparam = oparams.get(i);
+				String type = getParamTypeAsString(oparam);
+				String name = oparam.getName().toLowerCase();
+				con.append(type + " " + name);
+			}
+			con.append(") {\n");
+			for (int i = 0; i < oparams.size(); i++) {
+				DataIdentifier oparam = oparams.get(i);
+				String name = oparam.getName().toLowerCase();
+				con.append("this." + name + "=" + name + ";\n");
+			}
+			con.append("}\n");
+			String cstring = con.toString();
+			CtConstructor ctCon = CtNewConstructor.make(cstring, ctFuncOut);
+			ctFuncOut.addConstructor(ctCon);
+
+			StringBuilder s = new StringBuilder();
+			s.append("public String toString(){\n");
+			s.append("StringBuilder sb = new StringBuilder();\n");
+			for (int i = 0; i < oparams.size(); i++) {
+				DataIdentifier oparam = oparams.get(i);
+				String name = oparam.getName().toLowerCase();
+				s.append("sb.append(\"" + name + " (" + getSimpleParamTypeAsString(oparam) + "): \" + " + name
+						+ " + \"\\n\");\n");
+			}
+			s.append("String str = sb.toString();\nreturn str;\n");
+			s.append("}\n");
+			String toStr = s.toString();
+			CtMethod toStrMethod = CtNewMethod.make(toStr, ctFuncOut);
+			ctFuncOut.addMethod(toStrMethod);
+
+			ctFuncOut.writeFile(destination);
+
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		} catch (CannotCompileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String generateFunctionCallMethodNew(String scriptFilePath, FunctionStatement fs,
+			String dmlFunctionCall) {
+
+		createFunctionOutputClass(scriptFilePath, fs);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("public ");
+
+		// begin return type
+		ArrayList<DataIdentifier> oparams = fs.getOutputParams();
+		if (oparams.size() == 0) {
+			sb.append("void");
+		} else if (oparams.size() == 1) {
+			DataIdentifier oparam = oparams.get(0);
+			String type = getParamTypeAsString(oparam);
+			sb.append(type);
+		} else {
+			// sb.append("org.apache.sysml.api.mlcontext.MLResults");
+			String fullFunctionOutputClassName = getFullFunctionOutputClassName(scriptFilePath, fs);
+			sb.append(fullFunctionOutputClassName);
+		}
+		sb.append(" ");
+		// end return type
+
+		sb.append(fs.getName());
+		sb.append("(");
+
+		ArrayList<DataIdentifier> inputParams = fs.getInputParams();
+		for (int i = 0; i < inputParams.size(); i++) {
+			if (i > 0) {
+				sb.append(", ");
+			}
+			DataIdentifier inputParam = inputParams.get(i);
+			/*
+			 * Note: Using Object is currently preferrable to using
+			 * datatype/valuetype to explicitly set the input type to
+			 * Integer/Double/Boolean/String since Object allows the automatic
+			 * handling of things such as automatic conversions from longs to
+			 * ints.
+			 */
+			sb.append("Object ");
+			sb.append(inputParam.getName());
+		}
+
+		sb.append(") {\n");
+		sb.append("String scriptString = \"" + dmlFunctionCall + "\";\n");
+		sb.append(
+				"org.apache.sysml.api.mlcontext.Script script = new org.apache.sysml.api.mlcontext.Script(scriptString);\n");
+
+		// ArrayList<DataIdentifier> outputParams = fs.getOutputParams();
+		if ((inputParams.size() > 0) || (oparams.size() > 0)) {
+			sb.append("script");
+		}
+		for (int i = 0; i < inputParams.size(); i++) {
+			DataIdentifier inputParam = inputParams.get(i);
+			String name = inputParam.getName();
+			sb.append(".in(\"" + name + "\", " + name + ")");
+		}
+		for (int i = 0; i < oparams.size(); i++) {
+			DataIdentifier outputParam = oparams.get(i);
+			String name = outputParam.getName();
+			sb.append(".out(\"" + name + "\")");
+		}
+		if ((inputParams.size() > 0) || (oparams.size() > 0)) {
+			sb.append(";\n");
+		}
+
+		sb.append("org.apache.sysml.api.mlcontext.MLResults results = script.execute();\n");
+		// sb.append("return results;\n");
+
+		if (oparams.size() == 0) {
+			sb.append("return;\n");
+		} else if (oparams.size() == 1) {
+			DataIdentifier o = oparams.get(0);
+			DataType dt = o.getDataType();
+			ValueType vt = o.getValueType();
+			if ((dt == DataType.SCALAR) && (vt == ValueType.INT)) {
+				sb.append("long res = results.getLong(\"" + o.getName() + "\");\nreturn res;\n");
+			} else if ((dt == DataType.SCALAR) && (vt == ValueType.DOUBLE)) {
+				sb.append("double res = results.getDouble(\"" + o.getName() + "\");\nreturn res;\n");
+			} else if ((dt == DataType.SCALAR) && (vt == ValueType.BOOLEAN)) {
+				sb.append("boolean res = results.getBoolean(\"" + o.getName() + "\");\nreturn res;\n");
+			} else if ((dt == DataType.SCALAR) && (vt == ValueType.STRING)) {
+				sb.append("String res = results.getString(\"" + o.getName() + "\");\nreturn res;\n");
+			} else if (dt == DataType.MATRIX) {
+				sb.append("org.apache.sysml.api.mlcontext.Matrix res = results.getMatrix(\"" + o.getName()
+						+ "\");\nreturn res;\n");
+			} else if (dt == DataType.FRAME) {
+				sb.append("org.apache.sysml.api.mlcontext.Frame res = results.getFrame(\"" + o.getName()
+						+ "\");\nreturn res;\n");
+			}
+		} else {
+			// sb.append("return results;\n");
+
+			for (int i = 0; i < oparams.size(); i++) {
+				DataIdentifier outputParam = oparams.get(i);
+				String name = outputParam.getName().toLowerCase();
+				String type = getParamTypeAsString(outputParam);
+				// DataIdentifier o = oparams.get(0);
+				DataType dt = outputParam.getDataType();
+				ValueType vt = outputParam.getValueType();
+				if ((dt == DataType.SCALAR) && (vt == ValueType.INT)) {
+					sb.append(type + " " + name + " = results.getLong(\"" + outputParam.getName() + "\");\n");
+				} else if ((dt == DataType.SCALAR) && (vt == ValueType.DOUBLE)) {
+					sb.append(type + " " + name + " = results.getDouble(\"" + outputParam.getName() + "\");\n");
+				} else if ((dt == DataType.SCALAR) && (vt == ValueType.BOOLEAN)) {
+					sb.append(type + " " + name + " = results.getBoolean(\"" + outputParam.getName() + "\");\n");
+				} else if ((dt == DataType.SCALAR) && (vt == ValueType.STRING)) {
+					sb.append(type + " " + name + " = results.getString(\"" + outputParam.getName() + "\");\n");
+				} else if (dt == DataType.MATRIX) {
+					sb.append(type + " " + name + " = results.getMatrix(\"" + outputParam.getName() + "\");\n");
+				} else if (dt == DataType.FRAME) {
+					sb.append(type + " " + name + " = results.getFrame(\"" + outputParam.getName() + "\");\n");
+				}
+			}
+			String ffocn = getFullFunctionOutputClassName(scriptFilePath, fs);
+			sb.append(ffocn + " res = new " + ffocn + "(");
+			for (int i = 0; i < oparams.size(); i++) {
+				if (i > 0) {
+					sb.append(", ");
+				}
+				DataIdentifier outputParam = oparams.get(i);
+				String name = outputParam.getName().toLowerCase();
+				sb.append(name);
+			}
+			sb.append(");\nreturn res;\n");
+		}
+
 		sb.append("}\n");
 		return sb.toString();
 	}
